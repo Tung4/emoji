431039cda0c90508728f6f2cd975d0fd232a8a7c
TungPhi <46023194+TungPhi@users.noreply.github.com>
2020-02-20 17:30:53 +0700

    update code emoji

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
-import Header from './components/Header';
-import Body from './components/Body';
+import Header from './components/Header/Header'
+import Body from './components/Body/Body';
 
 function App() {
   const [showIcon, setShowIcon] = useState(false)
